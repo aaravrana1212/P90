766adc709dcaf5970583fdec82659aadebf0b63d
aaravrana1212 <96065093+aaravrana1212@users.noreply.github.com>
2022-06-26 18:08:57 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,12 @@
+function adduser() {
+    player1_name = document.getElementById("player1_name",player1_name).value;
+    player2_name = document.getElementById("player2_name",player2_name).value;
+
+    player1_name = document.getElementById("player1_name",player1_name).value
+    player2_name = document.getElementById("player2_name",player2_name).value
+
+
+    window.Location = "game_page.html";
+
+}
+
